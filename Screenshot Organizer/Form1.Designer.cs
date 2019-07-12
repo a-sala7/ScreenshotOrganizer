e5df07ff54e214d29a9b777376038d620c217144
a-sala7 <sovitpower4@gmail.com>
2019-07-12 18:02:59 +0200

    Add files via upload

---
+++ Screenshot Organizer/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Screenshot_Organizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.extensionsBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.fileNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.folderCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.extensionsCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.organizeBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EXE name (may be partial)";
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.Location = new System.Drawing.Point(12, 25);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.Size = new System.Drawing.Size(100, 20);
+            this.fileNameBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination Folder Name (full path if not in curr. directory)";
+            // 
+            // folderNameBox
+            // 
+            this.folderNameBox.Location = new System.Drawing.Point(12, 64);
+            this.folderNameBox.Name = "folderNameBox";
+            this.folderNameBox.Size = new System.Drawing.Size(276, 20);
+            this.folderNameBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(294, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Extensions (comma-separated) or write ALL for all extensions.";
+            // 
+            // extensionsBox
+            // 
+            this.extensionsBox.Location = new System.Drawing.Point(12, 103);
+            this.extensionsBox.Name = "extensionsBox";
+            this.extensionsBox.Size = new System.Drawing.Size(100, 20);
+            this.extensionsBox.TabIndex = 5;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(12, 131);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 6;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Location = new System.Drawing.Point(93, 131);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(103, 23);
+            this.removeBtn.TabIndex = 7;
+            this.removeBtn.Text = "Remove Selected";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            this.removeBtn.Click += new System.EventHandler(this.removeBtn_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileNameCol,
+            this.folderCol,
+            this.extensionsCol});
+            this.listView1.Location = new System.Drawing.Point(12, 160);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(444, 147);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // fileNameCol
+            // 
+            this.fileNameCol.Text = "File Name";
+            this.fileNameCol.Width = 111;
+            // 
+            // folderCol
+            // 
+            this.folderCol.Text = "Folder";
+            this.folderCol.Width = 206;
+            // 
+            // extensionsCol
+            // 
+            this.extensionsCol.Text = "Extensions";
+            this.extensionsCol.Width = 123;
+            // 
+            // organizeBtn
+            // 
+            this.organizeBtn.Location = new System.Drawing.Point(381, 131);
+            this.organizeBtn.Name = "organizeBtn";
+            this.organizeBtn.Size = new System.Drawing.Size(75, 23);
+            this.organizeBtn.TabIndex = 8;
+            this.organizeBtn.Text = "Organize It!";
+            this.organizeBtn.UseVisualStyleBackColor = true;
+            this.organizeBtn.Click += new System.EventHandler(this.organizeBtn_Click);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(294, 64);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(66, 20);
+            this.browseBtn.TabIndex = 10;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 319);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.organizeBtn);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.extensionsBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.folderNameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileNameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Screenshot Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox fileNameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox folderNameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox extensionsBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button removeBtn;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button organizeBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.ColumnHeader fileNameCol;
+        private System.Windows.Forms.ColumnHeader folderCol;
+        private System.Windows.Forms.ColumnHeader extensionsCol;
+    }
+}
+
---
+++ Screenshot Organizer/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Screenshot_Organizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void readList()
+        {
+            using (StreamReader reader = new StreamReader("organizer.cfg", true))
+            {
+                string currLine = reader.ReadLine();
+                while (currLine != null)
+                {
+                    string[] currData = currLine.Split(',');
+                    ListViewItem currItem = new ListViewItem(currData[0]);
+                    currItem.SubItems.Add(currData[1]);
+                    string exts = "";
+                    int sz = currData.Length;
+                    for (int i = 2; i < sz; i++)
+                    {
+                        if (i == sz - 1)
+                            exts += currData[i];
+                        else
+                        {
+                            exts += currData[i];
+                            exts += ',';
+                        }
+                    }
+                    currItem.SubItems.Add(exts);
+                    listView1.Items.Add(currItem);
+                    currLine = reader.ReadLine();
+                }
+            }
+        }
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                folderNameBox.Text = fbd.SelectedPath;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("organizer.cfg"))
+            {
+                File.Create("organizer.cfg").Close();
+            }
+            readList();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(fileNameBox.Text) || string.IsNullOrWhiteSpace(folderNameBox.Text) || string.IsNullOrWhiteSpace(extensionsBox.Text))
+                return;
+            else
+            {
+                using (StreamWriter writer = new StreamWriter("organizer.cfg", true))
+                {
+                    writer.Write("{0},{1},", fileNameBox.Text, folderNameBox.Text);
+                    if (extensionsBox.Text.ToLower() != "all")
+                    {
+                        string[] exts = extensionsBox.Text.Split(',');
+                        int sz = exts.Length;
+                        for (int i = 0; i < sz; i++)
+                        {
+                            if (i == sz - 1)
+                                writer.Write(exts[i]);
+                            else
+                                writer.Write(exts[i] + ',');
+                        }
+                        writer.WriteLine();
+                    }
+                    else
+                    {
+                        writer.WriteLine("all");
+                    }
+                }
+                listView1.Items.Clear();
+                readList();
+                fileNameBox.Clear();
+                folderNameBox.Clear();
+                extensionsBox.Clear();
+            }
+        }
+
+        private void organizeBtn_Click(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader("organizer.cfg", true))
+            {
+                string currLine = reader.ReadLine();
+                while (currLine != null)
+                {
+                    string[] currData = currLine.Split(',');
+                    int sz = currData.Length;
+                    string destination;
+                    if (currData[1].Contains(':'))
+                        destination = currData[1];
+                    else
+                        destination = @".\" + currData[1] + @"\";
+                    if (currData[2] == "all")
+                    {
+                        string[] filesToMove = Directory.GetFiles(@".\", currData[0] + "*.*");
+                        if (filesToMove.Length > 0 && !Directory.Exists(destination))
+                        {
+                            Directory.CreateDirectory(destination);
+                        }
+                        foreach (string f in filesToMove)
+                        {
+                            File.Move(f, destination + f);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 2; i < sz; i++)
+                        {
+                            string[] filesToMove = Directory.GetFiles(@".\", currData[0] + "*." + currData[i]);
+                            if (filesToMove.Length > 0 && !Directory.Exists(destination))
+                            {
+                                Directory.CreateDirectory(destination);
+                            }
+                            foreach (string f in filesToMove)
+                            {
+                                File.Move(f, destination + f);
+                            }
+                        }
+                    }
+                    currLine = reader.ReadLine();
+                }
+            }
+        }
+
+        private void removeBtn_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count < 1)
+                return;
+            List<string> linesToRemove = new List<string>();
+            foreach (ListViewItem x in listView1.SelectedItems)
+            {
+                linesToRemove.Add(string.Format("{0},{1},{2}", x.SubItems[0].Text, x.SubItems[1].Text, x.SubItems[2].Text));
+            }
+            using (StreamReader reader = new StreamReader("organizer.cfg", true))
+            {
+                using (StreamWriter writer = new StreamWriter("temp.cfg", true))
+                {
+                    string currLine = reader.ReadLine();
+                    while (currLine != null)
+                    {
+                        bool removing = false;
+                        if (linesToRemove.Contains(currLine))
+                            removing = true;
+                        if (!removing)
+                        {
+                            writer.WriteLine(currLine);
+                        }
+                        currLine = reader.ReadLine();
+                    }
+                }
+            }
+            File.Delete("organizer.cfg");
+            File.Move("temp.cfg", "organizer.cfg");
+            listView1.Items.Clear();
+            readList();
+        }
+    }
+}
